0fd7b30d1776e01be63827206ce946089128aaed
Joan Killeen <joan.killeen@bluewin.ch>
2017-05-14 15:53:21 +0200

    basic Lambda

---
+++ src/main/java/lambda/LambdaMain.java
@@ -0,0 +1,12 @@
+package lambda;
+
+public class LambdaMain {
+
+	public static void main(String[] args) {
+		MyNumber myNum;
+		myNum = ()-> 5.55;
+		System.out.println(myNum.getValue());
+
+	}
+
+}
---
+++ src/main/java/lambda/MyNumber.java
@@ -0,0 +1,6 @@
+package lambda;
+
+public interface MyNumber {
+	double getValue();
+
+}
